1c768ba53b3291e5f29a014be2253df6b0d5835b
Charliemhh <charles.montell-hall@ten10.com>
2023-10-09 11:42:07 +0100

    Version 0.3 Added another Unit Test

--- src/test/java/GameMapTests.java
+++ src/test/java/GameMapTests.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class GameMapTests {
     //An example of how the constructor will create a map object.
 //                          0       1       2       3       4    5
@@ -13,7 +15,23 @@ public class GameMapTests {
         GameMap testMap = new GameMap(3, connections);
         //Assert
         System.out.println(testMap.getMap().size());
-        assert(testMap.getMap().size()==3);
+        assert (testMap.getMap().size() == 3);
         //Expect 3 as there is room 0,1 and 2
+    }
+
+    @Test
+    void MapContentsShouldBeCorrect() {
+        //Arrange
+        int[][] connections = {{1, 2}, {0, 2}, {0, 1}};
+        ArrayList<Integer> testIndex2 = new ArrayList<>();
+        testIndex2.add(0);
+        testIndex2.add(1);
+        //Act
+        GameMap testMap = new GameMap(3, connections);
+        //Assert
+        assert (testMap.getMap().get(2).containsAll(testIndex2));
+        System.out.println(testMap.getMap().get(2));
+        System.out.println(testIndex2);
+        //Expecting 0, 1
     }
 }
