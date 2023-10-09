18a5929eaf7f9a9a9de2e8a4e1ff6b60abc4608a
Charliemhh <charles.montell-hall@ten10.com>
2023-10-09 12:18:52 +0100

    Version 0.3 Added CRI and get adjacent rooms methods + unit tests

--- src/main/java/GameMap.java
+++ src/main/java/GameMap.java
@@ -4,6 +4,9 @@ import java.util.LinkedList;
 public class GameMap {
     //Class representing the connections between rooms.
     private final ArrayList<LinkedList<Integer>> Map;
+
+    private int currentRoomIndex;
+
     public GameMap(int numberOfRooms, int[][] connections) {
         ArrayList<LinkedList<Integer>> Map = new ArrayList<>();
         for (int i = 0; i < numberOfRooms; i++) {
@@ -13,10 +16,23 @@ public class GameMap {
             }
         }
         this.Map = Map;
-        //the above loops create a new linked list for each room, then fill the list
+        this.currentRoomIndex = 0; //Set to first room by default
+        //the above loops create a new linked list for each room, then fill said list
         //with the "adjacent" rooms by adding to linked list.
     }
+
     public ArrayList<LinkedList<Integer>> getMap(){
         return this.Map;
+    }
+    public int getCurrentRoomIndex() {
+        return currentRoomIndex;
+    }
+
+    public void setCurrentRoomIndex(int currentRoomIndex) {
+        this.currentRoomIndex = currentRoomIndex;
+    }
+
+    public LinkedList<Integer> getAdjacentRooms(){
+        return this.Map.get(this.currentRoomIndex);
     }
 }
--- src/test/java/GameMapTests.java
+++ src/test/java/GameMapTests.java
@@ -1,20 +1,15 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class GameMapTests {
-    //An example of how the constructor will create a map object.
-//                          0       1       2       3       4    5
-//int connections[][] = { {1, 2}, {0,4}, {0,3,5}, {2,4}, {1,3}, {2} };
-//GameMap map1 = new GameMap(4, connections[][])
+
+    int[][] connections = {{1, 2}, {0, 2}, {0, 1}};
+    GameMap testMap = new GameMap(3, connections);
+
     @Test
     void MapShouldBeCorrectSize() {
-        //Arrange
-        int[][] connections = {{1, 2}, {0, 2}, {0, 1}};
-        //Act
-        GameMap testMap = new GameMap(3, connections);
-        //Assert
-        System.out.println(testMap.getMap().size());
         assert (testMap.getMap().size() == 3);
         //Expect 3 as there is room 0,1 and 2
     }
@@ -22,7 +17,6 @@ public class GameMapTests {
     @Test
     void MapContentsShouldBeCorrect() {
         //Arrange
-        int[][] connections = {{1, 2}, {0, 2}, {0, 1}};
         ArrayList<Integer> testIndex2 = new ArrayList<>();
         testIndex2.add(0);
         testIndex2.add(1);
@@ -30,8 +24,33 @@ public class GameMapTests {
         GameMap testMap = new GameMap(3, connections);
         //Assert
         assert (testMap.getMap().get(2).containsAll(testIndex2));
-        System.out.println(testMap.getMap().get(2));
-        System.out.println(testIndex2);
         //Expecting 0, 1
+    }
+
+    @Test
+    void defaultAdjacentRoomShouldBeCorrect() {
+        //Arrange
+        LinkedList<Integer> testIndex0 = new LinkedList<>();
+        testIndex0.add(1);
+        testIndex0.add(2);
+        //Act
+        GameMap testMap = new GameMap(3, connections);
+        //Assert
+        assert (testIndex0.containsAll(testMap.getAdjacentRooms()));
+        //Expect 1 and 2 for location 0
+
+
+    }
+    @Test
+    void newAdjacentRoomShouldBeCorrect(){
+        //Arrange
+        LinkedList<Integer> testIndex1 = new LinkedList<>();
+        testIndex1.add(0);
+        testIndex1.add(2);
+        //Act
+        testMap.setCurrentRoomIndex(1);
+        //Assert
+        assert (testIndex1.containsAll(testMap.getAdjacentRooms()));
+        //Expect 0 and 2 for location 1 - imagine the player has moved
     }
 }
