908fe03d36be933a6991c343c444a8591e029301
Charliemhh <charles.montell-hall@ten10.com>
2023-10-10 15:40:14 +0100

    Version 0.17 added Trap class

---
+++ src/main/java/Trap.java
@@ -0,0 +1,58 @@
+public class Trap {
+    private final int trapID;
+    private boolean activated;
+    private final String activeDescription;
+    private final String discoveryDescription;
+    private final int trapEffects;
+
+    public Trap(int trapID, String activationDescription, String discoveryDescription, int trapEffects) {
+        this.trapID = trapID;
+        this.activated = false;
+        this.activeDescription = activationDescription;
+        this.discoveryDescription = discoveryDescription;
+        this.trapEffects = trapEffects;
+    }
+
+    public int trapActivate(Trap trap) {
+        this.activated = true;
+        return this.trapEffects;
+    }
+
+    public int getTrapEffects() {
+        return trapEffects;
+    }
+
+    public String getActiveDescription() {
+        return activeDescription;
+    }
+
+    public String getDiscoveryDescription() {
+        return discoveryDescription;
+    }
+
+    public boolean isActivated() {
+        return activated;
+    }
+
+    public int getTrapID() {
+        return trapID;
+    }
+
+    public static class TrapCreator {
+
+        public static void createTrap(int id) {
+            switch (id) {
+                case 0://Spike Trap
+                    Trap spikeTrap = new Trap(id,
+                            "Spikes suddenly erupt from the ground!"
+                            , "A small tripwire leads to a set of spikes, primed to launch.",
+                            5);
+                case 1://Log Trap
+                    Trap logTrap = new Trap(id,
+                            "A series of large heavy logs roll towards you, threatening to bowl you over",
+                            "A container of heavy logs threatens to be unleashed if one steps on the wrong panel.",
+                            8);
+            }
+        }
+    }
+}
